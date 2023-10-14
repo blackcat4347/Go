af6634a065a73bcb4e74ca5f0272aa9e787912e1
blackcat4347 <chihuong195195@gmail.com>
2023-10-14 20:41:31 +0700

    Update foodController

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -124,7 +124,67 @@ func toFixed(num float64, precision int) float64 {
 
 func UpdateFood() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		var ctx, cancel = context.WithTimeout(contex.Background())
+		var ctx, cancel = context.WithTimeout(contex.Background(), 100*time.Second)
+		var menu models.Menu
+		var food models.Food
+
+		foodId := c.Param("food_id")
+
+		if err := c.BindJSON(&food); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		
+		var updateObj primitive.D
+
+		if food.Name != nil {
+			updateObj = append(updateObj, bson.E{"name", food.Name})
+		}
+
+		if food.Price != nil {
+			updateObj = append(updateObj, bson.E{"price", food.Price})
+		}
+
+		if food.Food_image != nil {
+			updateObj = append(updateObj, bson.E{"food_image", food.Food_image})
+		}
+
+		if food.Menu_id != nil {
+			err := menuCollection.FineOne(ctx, bson.M{"menu_id": food.Menu.id}).Decode(&menu)
+			defer cancel()
+			if err != nil {
+				msg := fmt.Sprintf("message:Menu was not found")
+				c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+				return
+			}
+			updateObj = append(updateObj, bson.E{"menu", food.Price})
+		}
+		food.Update_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updateObj = append(updateObj, bson.E{"update_at": food.Update_at})
+
+		upsert := true
+		filter := bson.M{"food_id": foodId}
+
+		opt := options.UpdateOtions{
+			Upsert: &upsert,
+		}
+
+		result, err := foodCollection.UpdateOne{
+			ctx,
+			filter,
+			bson.D{
+				{"$set", updateObj}
+			},
+			&opt,
+		}
+
+		if err != nil {
+			msg := fmt.Sprint("food item update failed")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		c.JSON(http.StatusOK, result)
 
 	}
 }
