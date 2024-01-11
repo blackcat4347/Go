ea05e5008e5b1cdc7fbb4c4cf3b80e37b1611eab
blackcat4347 <chihuong195195@gmail.com>
2024-01-11 20:47:02 +0700

    update tablecontrol

--- controllers/orderItermController.go
+++ controllers/orderItermController.go
@@ -1,20 +1,63 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/models"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type OrderItemPack struct {
+	Table_id    *string
+	Order_items []models.OrderItem
+}
+
+var orderItemCollection *mongo.Collection = database.OpenCollection(database.Client, "orderItem")
 
 func GetOrderItems() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		result, err := orderItemCollection.Find(context.TODO(), bson.M{})
+
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing ordered items"})
+			return
+		}
+		var allOrderItems []bson.M
+		if err = result.All(ctx, &allOrderItems); err != nil {
+			log.Fatal(err)
+			return
+		}
+		c.JSON(http.StatusOK, allOrderItems)
 	}
 }
 func GetOrderItemsByOrder() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		orderId := c.Param("order_id")
+
+		allOrderItems, err := ItemsByOrder(orderId)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing order items by order ID"})
+			return
+		}
+		c.JSON(http.StatusOK, allOrderItems)
 
 	}
 }
 
 func ItemsByOrder(id string) (OrderItems []primitve.M, err error) {
-
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 }
 func GetOrderItem() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
